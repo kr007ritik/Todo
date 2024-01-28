b520fbff51ae11128528ad360b3fe8d7bfa07488
kr007ritik <kumar007ritik@gmail.com>
2024-01-29 01:02:04 +0530

    Added local storage, task will not be deleted.

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-const taskInput = document.getElementById("taskInput");
-const addTaskBtn = document.getElementById("addTaskBtn");
-const taskList  = document.getElementById("taskList");
+const inputBox = document.getElementById("inputBox");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
-addTaskBtn.addEventListener('click',function() {
-    const taskText = taskInput.value.trim();
+addBtn.addEventListener("click", function () {
+  const taskText = inputBox.value.trim();
 
-    if(taskText !== ""){
-        addTask(taskText);
-        taskInput.value = "";
-    }
+  if (taskText !== "") {
+    addTask(taskText);
+    inputBox.value = "";
+    saveData();
+  }
 });
 
-function addTask(taskText){
-    const li = document.createElement("li");
-    const taskTextElement = document.createElement("span");
-    const deleteBtn = document.createElement("button");
+function addTask(taskText) {
+  const li = document.createElement("li");
+  const task = document.createElement("span");
+  const deleteBtn = document.createElement("button");
 
-    taskTextElement.textContent = taskText;
-    deleteBtn.textContent = "Delete";
+  task.textContent = taskText;
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList = "deleteBtn";
+  li.classList = "notComplted";
 
-    li.appendChild(taskTextElement);
-    li.appendChild(deleteBtn);
-    taskList.appendChild(li);
-
-    deleteBtn.addEventListener("click", function () {
-        li.remove();
-    });
-        
-    li.addEventListener("click", function () {
-        li.classList.toggle("completed");
-        // deleteBtn.textContent = "✅ Task Completed 😍";
-    });
-        
+  taskList.appendChild(li);
+  li.appendChild(task);
+  li.appendChild(deleteBtn);
 }
 
+taskList.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("completed");
+    e.target.classList.toggle("notComplted");
+    saveData();
+    showTask();
+  } else if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+    saveData();
+    showTask();
+  }
+},false);
 
+function saveData() {
+  localStorage.setItem("data", taskList.innerHTML);
+}
 
+function showTask() {
+  taskList.innerHTML = localStorage.getItem("data");
+}
+showTask();
